10f44e2a5daf6d7191e5827f192d7b1a88ca31f6
Sean Cairns <sean.cairnsst@gmail.com>
2026-02-08 20:56:58 +0000

    Update default.nix

--- default.nix
+++ default.nix
@@ -10,41 +10,44 @@
 , cdrkit
 , p7zip
 }:
-
 stdenv.mkDerivation rec {
   pname = "uup-converter";
   version = "latest";
-
+  
   src = fetchgit {
     url = "https://git.uupdump.net/uup-dump/converter.git";
     rev = "HEAD";
     sha256 = "sha256-1B0zxoN4FnQExpzy8mV+/ih70ze1jgtVjCyOZ6Kn4Wc=";
   };
-
+  
   nativeBuildInputs = [
     makeWrapper
-  ];
-
-  buildInputs = [
     bash
     aria2
     cabextract
     wimlib
     chntpw
-    cdrkit   # genisoimage
+    cdrkit
     p7zip
   ];
-
+  
   dontBuild = true;
-
+  
   installPhase = ''
     mkdir -p $out/bin
     install -m755 convert.sh $out/bin/uup-converter
-
     wrapProgram $out/bin/uup-converter \
-      --prefix PATH : ${lib.makeBinPath buildInputs}
+      --prefix PATH : ${lib.makeBinPath [
+        aria2
+        cabextract
+        wimlib
+        chntpw
+        cdrkit
+        p7zip
+        bash
+      ]}
   '';
-
+  
   meta = with lib; {
     description = "UUP Dump Converter – build Windows ISOs from UUP files";
     homepage = "https://git.uupdump.net/uup-dump/converter";
